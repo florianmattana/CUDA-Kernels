e57c7b02b10055427cde2c441209e8b3cda89d78
florianmattana <mattana.florian@gmail.com>
2026-01-28 14:22:26 +0100

    gemm optimised kernel v1

--- matrix_multiplication.cu
+++ matrix_multiplication.cu
@@ -41,12 +41,66 @@ __global__ void naive_gemm(const float* A, const float* B, float* C, int M, int 
     }
 }
 
+// Block Tiling dimensions for KERNEL B
+    constexpr int BM = 16;
+    constexpr int BK = 16;
+    constexpr int BN = 16;
+
+__global__ void tiled_gemm_shared(const float* A, const float* B, float* C, int M, int N, int K)
+{
+    int baseRow = blockIdx.y * BM;
+    int baseCol = blockIdx.x * BN;
+
+    int row = baseRow + threadIdx.y; 
+    int col = baseCol + threadIdx.x;
+
+    __shared__ float As[BM][BK]; //tuile de A stockée en shared de taille BM×BK
+    __shared__ float Bs[BK][BN]; //tuile de A stockée en shared de taille BK×BM
+
+    float tmp = 0.0f;
+
+    // 3) Parcours de K par tranches de BK
+    for (int k0 = 0; k0 < K; k0 += BK)
+    {
+        
+        int aRow = row;                 // ligne de A = ligne de C
+        int aCol = k0 + threadIdx.x;    // colonne de A dans la tranche K
+        int bRow = k0 + threadIdx.y;    // ligne de B dans la tranche K
+        int bCol = col;                 // colonne de B = colonne de C
+
+        As[threadIdx.y][threadIdx.x] = 
+            (aRow < M && aCol < K) ? A[aRow * K + aCol] : 0.0f;
+
+        Bs[threadIdx.y][threadIdx.x] = 
+            (bRow < K && bCol < N) ? B[bRow * N + bCol] : 0.0f;
+
+        __syncthreads();
+    
+        // 5) Calcul sur BK (produit scalaire partiel)
+        #pragma unroll
+        for (int kk = 0; kk < BK; ++kk)
+        {
+            tmp += As[threadIdx.y][kk] * Bs[kk][threadIdx.x];
+        }
+        __syncthreads();
+    }
+        if (row < M && col < N)
+        {
+            C[row * N + col] = tmp;
+        }
+};
+
 int main()
 {
-
+// ================================ GENERAL SET UP =============================================
+    // A = M * K
+    // B = K * N 
+    // C = M * N 
     const int M = 1000; // A rows, C rows
     const int K = 512;  // A cols, B rows
     const int N = 2000; // B cols, C cols
+
+// ================================ Memory Set up ==============================================
 
     size_t elemsA = static_cast<size_t>(M) * K;
     size_t elemsB = static_cast<size_t>(K) * N;
@@ -70,6 +124,8 @@ int main()
 
     CUDA_CHECK(cudaMemcpy(dA, hA, bytesA, cudaMemcpyHostToDevice));
     CUDA_CHECK(cudaMemcpy(dB, hB, bytesB, cudaMemcpyHostToDevice));
+    
+    // ================================ KERNEL 1 ===================================================
 
     dim3 threads(16, 16, 1);
     dim3 blocks((N + threads.x - 1) / threads.x,
@@ -81,20 +137,53 @@ int main()
     CUDA_CHECK(cudaEventCreate(&stop));
 
     CUDA_CHECK(cudaEventRecord(start));
+    
     naive_gemm<<<blocks, threads>>>(dA, dB, dC, M, N, K);
+    
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaEventRecord(stop));
     CUDA_CHECK(cudaEventSynchronize(stop));
-
+    
     float kernel_1 = 0.0f;
     CUDA_CHECK(cudaEventElapsedTime(&kernel_1, start, stop));
-
+    
     CUDA_CHECK(cudaMemcpy(hC, dC, bytesC, cudaMemcpyDeviceToHost));
+    
+    // ================================ KERNEL 2 ===================================================
+    
+    dim3 threads_2(16, 16, 1);
+    dim3 blocks_2(
+        (N + BN - 1) / BN,
+        (M + BM - 1) / BM,
+        1
+    );
+    
+    float kernel_2 = 0.0f;
+    
+    cudaEvent_t start_2, stop_2;
+    CUDA_CHECK(cudaEventCreate(&start_2));
+    CUDA_CHECK(cudaEventCreate(&stop_2));
 
+    CUDA_CHECK(cudaEventRecord(start_2));
+    
+    tiled_gemm_shared<<<blocks_2,threads_2 >>>(dA, dB, dC, M, N, K); 
+
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaEventRecord(stop_2));
+    CUDA_CHECK(cudaEventSynchronize(stop_2));
+
+    CUDA_CHECK(cudaEventElapsedTime(&kernel_2, start_2, stop_2));
+    
+    // ================================ Display Results ============================================
     std::cout << "Naive GEMM time: "<< kernel_1 <<"ms"<< std::endl;
+    std::cout << "GEMM Tiled Kernel time: "<< kernel_2 <<"ms"<< std::endl;
+    
+    // ================================ Memory Free ================================================
 
     CUDA_CHECK(cudaEventDestroy(start));
+    CUDA_CHECK(cudaEventDestroy(start_2));
     CUDA_CHECK(cudaEventDestroy(stop));
+    CUDA_CHECK(cudaEventDestroy(stop_2));
 
     CUDA_CHECK(cudaFree(dA));
     CUDA_CHECK(cudaFree(dB));
