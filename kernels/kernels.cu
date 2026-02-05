6047ffd43e73aa4aeb9e4c1a7c2d5ed3250e0c89
florianmattana <mattana.florian@gmail.com>
2026-02-05 16:40:31 +0100

    quick formating

--- kernels/kernels.cu
+++ kernels/kernels.cu
@@ -19,22 +19,22 @@ __global__ void naive_gemm(const float* A, const float* B, float* C, int M, int 
 
 __global__ void tiled_gemm(float* matrix_A, float* matrix_B, float* matrix_C, int M, int K, int N)
 {
-    int col = blockIdx.x * BN + threadIdx.x;
-    int row = blockIdx.y * BM + threadIdx.y;
+    int idx_tx = blockIdx.x * BN + threadIdx.x;
+    int idx_ty = blockIdx.y * BM + threadIdx.y;
 
     __shared__ float At[BM][BK];
     __shared__ float Bt[BK][BN];
 
-    __syncthreads();
-
     float tmp = 0.0f;
 
     for (int k0 = 0; k0 < K; k0 += BK)
-    {
-        int a_row = row;
+    { 
+
+        int a_row = idx_ty;
         int a_col = k0 + threadIdx.x;
-        int b_col = col;
-        int b_row = k0 + threadIdx.y;
+
+        int b_col = idx_tx;
+        int b_row = k0 + threadIdx .y;
 
         At[threadIdx.y][threadIdx.x] = (a_row < M && a_col < K) ? matrix_A[a_row * K + a_col] : 0.0f;
         Bt[threadIdx.y][threadIdx.x] = (b_row < K && b_col < N) ? matrix_B[b_row * N + b_col] : 0.0f;
@@ -48,8 +48,32 @@ __global__ void tiled_gemm(float* matrix_A, float* matrix_B, float* matrix_C, in
         }
         __syncthreads();
     }
-    if (row < M && col < N)
+    if (idx_ty < M && idx_tx < N)
     {
-        matrix_C[row * N + col] = tmp;
+        matrix_C[idx_ty * N + idx_tx] = tmp;
     }
+};
+
+__global__ void tiled_gemm_upgrd(const float* matrix_A, const float* matrix_B, float* matrix_C, int M, int K, int N, const int TM)
+{
+    int col = blockIdx.x * BN + threadIdx.x;
+    int row = blockIdx.y * BM + threadIdx.y * TM;
+
+    __shared__ float At[BM][BK];
+    __shared__ float Bt[BK][BN];
+
+    __syncthreads();
+
+    float acc[TM] = { 0.0f };
+
+#pragma unroll
+    for (int k0 = 0; k0 < K; k0 += BK)
+    {
+#pragma unroll
+        for (int i = 0; i < TM; ++i)
+        {
+            const int a_row = row + i;
+
+
+        };
 };
--- kernels/kernels.h
+++ kernels/kernels.h
@@ -9,3 +9,6 @@ constexpr int BK = 16;
 constexpr int BN = 16;
 
 __global__ void tiled_gemm(float* matrix_A, float* matrix_B, float* matrix_C, int M, int K, int N);
+
+template<int TM>
+__global__ void tiled_gemm_upgrd(const float* matrix_A, const float* matrix_B, float* matrix_C, int M, int K, int N);
