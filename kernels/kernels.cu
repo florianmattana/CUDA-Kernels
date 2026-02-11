4079b843f6c64ab0a9e8bbe1d56c888a021ef1f6
florianmattana <mattana.florian@gmail.com>
2026-02-11 18:04:21 +0100

    tiled_gemm_upgrd strategy changed

--- kernels/kernels.cu
+++ kernels/kernels.cu
@@ -1,79 +1,130 @@
-#include "cuda_intellisense_fix.h"
-#include "kernels.h"
+#include"cuda_intellisense_fix.h"
+#include"kernels.h"
 #include<cuda_runtime.h>
 
 __global__ void naive_gemm(const float* A, const float* B, float* C, int M, int K, int N)
 {
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
+    int threadId_x = blockDim.x * blockIdx.x + threadIdx.x;
+    int threadId_y = blockDim.y * blockIdx.y + threadIdx.y;
 
-    if (row < M && col < N)
+    if (threadId_y < M && threadId_x < N)
     {
-        float tmp = 0.0f;
+        float total = 0.0f;
+
         for (int i = 0; i < K; ++i)
-            tmp += A[row * K + i] * B[i * N + col];
-
-        C[row * N + col] = tmp;
+        {
+            total += A[threadId_y * K + i] * B[i * N + threadId_x];
+        }
+        C[threadId_y * N + threadId_x] = total;
     }
-}
+};
 
-__global__ void tiled_gemm(float* matrix_A, float* matrix_B, float* matrix_C, int M, int K, int N)
+__global__ void tiled_gemm(float* A, float* B, float* C, int M, int K, int N)
 {
-    int idx_tx = blockIdx.x * BN + threadIdx.x;
-    int idx_ty = blockIdx.y * BM + threadIdx.y;
+    int threadId_x = blockIdx.x * BN + threadIdx.x;
+    int threadId_y = blockIdx.y * BM + threadIdx.y;
 
-    __shared__ float At[BM][BK];
-    __shared__ float Bt[BK][BN];
+    __shared__ float As[BM][BK];
+    __shared__ float Bs[BK][BN];
 
-    float tmp = 0.0f;
+    float total = 0.0f;
 
     for (int k0 = 0; k0 < K; k0 += BK)
-    { 
+    {
 
-        int a_row = idx_ty;
+        int a_row = threadId_y;
         int a_col = k0 + threadIdx.x;
 
-        int b_col = idx_tx;
-        int b_row = k0 + threadIdx .y;
+        int b_col = threadId_x;
+        int b_row = k0 + threadIdx.y;
 
-        At[threadIdx.y][threadIdx.x] = (a_row < M && a_col < K) ? matrix_A[a_row * K + a_col] : 0.0f;
-        Bt[threadIdx.y][threadIdx.x] = (b_row < K && b_col < N) ? matrix_B[b_row * N + b_col] : 0.0f;
+        As[threadIdx.y][threadIdx.x] = (a_row < M && a_col < K) ? A[a_row * K + a_col] : 0.0f;
+        Bs[threadIdx.y][threadIdx.x] = (b_row < K && b_col < N) ? B[b_row * N + b_col] : 0.0f;
 
         __syncthreads();
 
 #pragma unroll
         for (int kk = 0; kk < BK; ++kk)
         {
-            tmp += At[threadIdx.y][kk] * Bt[kk][threadIdx.x];
+            total += As[threadIdx.y][kk] * Bs[kk][threadIdx.x];
         }
         __syncthreads();
     }
-    if (idx_ty < M && idx_tx < N)
+    if (threadId_y < M && threadId_x < N)
     {
-        matrix_C[idx_ty * N + idx_tx] = tmp;
+        C[threadId_y * N + threadId_x] = total;
     }
 };
 
-__global__ void tiled_gemm_upgrd(const float* matrix_A, const float* matrix_B, float* matrix_C, int M, int K, int N, const int TM)
+template<int TM>
+__global__ void tiled_gemm_upgrd(const float* A, const float* B, float* C, int M, int K, int N)
 {
-    int col = blockIdx.x * BN + threadIdx.x;
-    int row = blockIdx.y * BM + threadIdx.y * TM;
+    int threadId_x = blockIdx.x * BN + threadIdx.x;
+    int threadId_y = blockIdx.y * BM + threadIdx.y * TM;
 
-    __shared__ float At[BM][BK];
-    __shared__ float Bt[BK][BN];
-
-    __syncthreads();
+    __shared__ float As[BM][BK];
+    __shared__ float Bs[BK][BN];
 
     float acc[TM] = { 0.0f };
 
-#pragma unroll
-    for (int k0 = 0; k0 < K; k0 += BK)
+    int num_threads = blockDim.x * blockDim.y;
+    int linear_id = threadIdx.y * blockDim.x + threadIdx.x;
+
+    for (int k0 = 0; k0 < K < ++k0)
     {
-#pragma unroll
-        for (int i = 0; i < TM; ++i)
-        {
-            const int a_row = row + i;
+
+    }
+}
 
 
-        };
-};
+
+//   for (int k0 = 0; k0 < K; k0 += BK)
+//    {
+//
+//#pragma unroll
+//        for (int i = 0; i < TM; ++i)
+//        {
+//            int aRow = rowBase + i;          // global row
+//            int aCol = k0 + threadIdx.x;     // global col in K
+//            int aRowLocal = threadIdx.y * TM + i; // 0..BM-1
+//
+//            At[aRowLocal][threadIdx.x] = (aRow < M && aCol < K) ? A[aRow * K + aCol] : 0.0f;
+//        }
+//
+//        // --- Load B tile (simple) ---
+//        // Ici threadIdx.y doit couvrir BK (donc blockDim.y == BK)
+//        int bRow = k0 + threadIdx.y;
+//        int bCol = col;
+//
+//        Bt[threadIdx.y][threadIdx.x] =
+//            (bRow < K && bCol < N) ? B[bRow * N + bCol] : 0.0f;
+//
+//        __syncthreads();
+//
+//        // --- Compute ---
+//#pragma unroll
+//        for (int kk = 0; kk < BK; ++kk)
+//        {
+//            float b = Bt[kk][threadIdx.x]; // même colonne
+//
+//#pragma unroll
+//            for (int i = 0; i < TM; ++i)
+//            {
+//                int aRowLocal = threadIdx.y * TM + i;
+//                acc[i] += At[aRowLocal][kk] * b;
+//            }
+//        }
+//
+//        __syncthreads();
+//    }
+//
+//    // --- Store TM résultats ---
+//    if (col < N)
+//    {
+//#pragma unroll
+//        for (int i = 0; i < TM; ++i)
+//        {
+//            int r = rowBase + i;
+//            if (r < M) C[r * N + col] = acc[i];
+//        }
+//    }
