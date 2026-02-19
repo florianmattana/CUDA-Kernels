26fcd536d40008bbdffb727204b40f31d2a91078
florianmattana <mattana.florian@gmail.com>
2026-02-19 10:51:18 +0100

    Supprime enc_temp_folder et ajout au .gitignore

--- enc_temp_folder/d8d7d1f1cc6b268b9dedac5f9e2c1a2c/test.cu
+++
@@ -1,8 +0,0 @@
-__global__ void tiled_GEMM(const float* A, const float* B, float* C, int M, int K, int N)
-{
-	int threadId_x = blockIdx.x * BN + threadIdx.x;
-	int threadId_y = blockIdx.y * BM + threadIdx.y;
-
-
-
-}
